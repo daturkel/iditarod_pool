63d9235dcda954d7e3d50528110da6562de075c3
daturkel <daturkel@gmail.com>
2014-03-02 01:34:27 -0500

    added readme, support for fixed random seeds, re-factored output into a hash

---
+++ README.md
@@ -0,0 +1,17 @@
+# Dan's Iditarod Betting Pool!
+
+## Welcome
+
+Welcome to the Github repo for Dan's Iditarod Betting Pool! In the interest of full disclosure, and since there's some *serious cash* on the line here, I figured I'd make the musher assignment process totally transparent for this betting pool. In this repo, you'll find a Ruby script `iditarod.rb` which is the exact script used to assign mushers to betters. The list of mushers, subdivided into regular mushers and rookie mushers (who have not raced before), is included as well in the form of `racers.yaml`. The `betters.yaml` file and a sample `output.yaml` file have been replaced with dummy versions to protect the privacy of those whose last names are in the real files.
+
+## Rationale
+
+My friends and I had an Iditarod betting pool in high school, but we did it draft pick style. The draft pick order was random, but the person who got first pick got to pick [Lance Mackey](http://en.wikipedia.org/wiki/Lance_Mackey), winner of the 2007-2010 Iditarod races, and it was totally unfair and lame (although he didn't end up winning!). I decided that in the future, the fairest way to distribute mushers would be to randomly assign them.
+
+The `iditarod.rb` script randomly divvies up the non-rookie mushers to the betters, and then divvies the rookies in reverse order so that betters towards the end of the list, who may have received one fewer musher than the betters at the beginning of the list, will get rookies first and thus possibly balance out the distribution. This should prevent any one better from having more than one musher than the person with the least number of mushers assigned.
+
+## Auditing
+
+It would be pretty easy for me to fake the output and stack the odds however I wanted! That's why I've included the ability for you to audit results if you're so inclined. Line 3 of `iditarod.rb` contains a declaration of the variable `random_seed` which is used to seed the random-number-generator which is then used to shuffle the musher array, rookie array, and better array before the mushers get divvied. When the script is run multiple times for the same random seed, the results are always the same.
+
+On the day of the divvying, we'll randomly choose a seed and I'll update the file here. I will post the real `betters.yaml` file to the Facebook group and you can run the script yourself to ensure that your results match mine. Running the script is simple. Download the `iditarod.rb` file, the `racers.yaml` file, and the `betters.yaml` file (the last one coming from the Facebook group). Ensure that the random seed in `iditarod.rb` is set to the final proper value. Then `cd` to the directory with these files in it in your terminal of choice and run `ruby iditarod.rb`. Then simply open the `output.yaml` file that gets created and compare it to the one I'll publish. You'll find that it's exactly the same.
--- iditarod.rb
+++ iditarod.rb
@@ -1,5 +1,8 @@
 require 'yaml' # enable the ability to parse YAML files
 
+random_seed = 123123123
+seeded_random = Random.new(random_seed)
+
 mushers = [] # initialize the musher array
 rookies = [] # initialize the rookie array
 
@@ -18,27 +21,28 @@ better_file["betters"].each do |k| # populate the betters array
     betters.push(k["name"])
 end
 
-mushers.shuffle! # randomize the order of the musher, rookie, and better arrays
-rookies.shuffle!
-betters.shuffle!
+mushers.shuffle!(random: seeded_random) # randomize the order of the musher, rookie, and better arrays
+rookies.shuffle!(random: seeded_random)
+betters.shuffle!(random: seeded_random)
 
-property = [] # initialize the property array, it will be an array of hashes which each represent a better and the mushers and rookies they "own"
-(0..betters.length-1).each do |l| # initialize the property array with the betters' names and create arrays for their mushers and rookies
-   property[l]={"name" => betters[l],"racers"=>{"mushers"=>[],"rookies"=>[]}}
+results = {"random_seed" => random_seed, "listings" => []} # initalize the results hash which contains the random seed and a listings array which will contain hashes of each better and the mushers/rookies they get assigned
+(0..betters.length-1).each do |l| # initialize the listings array with the betters' names and create arrays for their mushers and rookies
+   results["listings"][l]={"name" => betters[l],"racers"=>{"mushers"=>[],"rookies"=>[]}}
 end
 mushers.each_with_index do |m,x| # give the betters each mushers
     a = x%betters.length
-    property[a]["racers"]["mushers"].push(m)
+    results["listings"][a]["racers"]["mushers"].push(m)
 end
 
 rookies.each_with_index do |n,y| # give the betters each rookies
     e = y%betters.length
-    property[betters.length-e-1]["racers"]["rookies"].push(n) # this step goes through the property array backwards so that people at the end of the property array are more likely to get an additional rookie, balancing out the fact that they may have gotten one fewer musher
+    results["listings"][betters.length-e-1]["racers"]["rookies"].push(n) # this step goes through the results array backwards so that people at the end of the results array are more likely to get an additional rookie, balancing out the fact that they may have gotten one fewer musher
 end
 
-File.open("output.yaml", "w") { |file| file.write(property.to_yaml)} # output the results to a file
+File.open("output.yaml", "w+") { |file| file.write(results.to_yaml) } 
 
-property.each do |z| # and, for good measure, pretty-print the results too
+puts "random_seed = " + results["random_seed"].to_s
+results["listings"].each do |z| # and, for good measure, pretty-print the results too
     puts z["name"] + " - " + (z["racers"]["mushers"].length+z["racers"]["rookies"].length).to_s + " racers"
     puts "   " + z["racers"]["mushers"].length.to_s + " mushers:"
     z["racers"]["mushers"].each do |alpha|
